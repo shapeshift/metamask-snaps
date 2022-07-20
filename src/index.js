c76281ec80f3db9473b2cc456ef0b41ebcc2f422
pastaghost <62026038+pastaghost@users.noreply.github.com>
2022-07-20 12:55:43 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+module.exports.onRpcRequest = async ({ origin, request }) => {
+  switch (request.method) {
+    case 'hello':
+      return wallet.request({
+        method: 'snap_confirm',
+        params: [
+          {
+            prompt: `Hello, ${origin}!`,
+            description:
+              'This custom confirmation is just for display purposes.',
+            textAreaContent:
+              'But you can edit the snap source code to make it do something, if you want to!',
+          },
+        ],
+      });
+    default:
+      throw new Error('Method not found.');
+  }
+};
