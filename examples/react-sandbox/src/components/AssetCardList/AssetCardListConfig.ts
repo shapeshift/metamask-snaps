20286f1aff7c35eb35e425079f0154014931ae64
pastaghost <pastaghost@2520.io>
2022-07-30 05:07:55 -0600

    fix: update asset card config to match snap RPC calls

--- examples/react-sandbox/src/components/AssetCardList/AssetCardListConfig.ts
+++ examples/react-sandbox/src/components/AssetCardList/AssetCardListConfig.ts
@@ -7,102 +7,93 @@ import { CardActionProps, CardProps } from '../Card/Card'
 
 export const AssetCardListConfig: Array<CardProps> = [
   {
-    name: 'Bitcoin',
+    name: 'Bitcoin (BTC)',
     icon: 'btc.png',
     actions: new Map<string, CardActionProps>([
-      ['address', { callback: undefined, params: null, description: 'Generate a receive address' }],
       [
-        'sign',
+        'btc_getAddress',
+        { callback: undefined, params: null, description: 'Generate a receive address' },
+      ],
+      [
+        'btc_signMessage',
         {
-          callback: undefined,
+          callback: () => {
+            console.info('Just invoked the btc_signMessage callback!')
+          },
           params: BitcoinTransactions.sign,
-          description: 'Sign a demo message',
+          description: 'Sign a test message',
         },
       ],
       [
-        'tx',
+        'btc_signTransaction',
         {
           callback: () => {
-            console.info('Just invoked the BTC tx callback!')
+            console.info('Just invoked the btc_signTransaction callback!')
           },
           params: BitcoinTransactions.tx,
           description: 'Sign a test transaction',
         },
       ],
       [
-        'verify',
+        'btc_verifyTransaction',
         {
           callback: undefined,
           params: BitcoinTransactions.verify,
-          description: 'Verify the signature from a previously-signed demo message',
+          description: 'Verify the signature from a previously-signed test message',
         },
       ],
     ]),
   },
   {
-    name: 'Cosmos',
+    name: 'Cosmos (ATOM)',
     icon: 'atom.png',
     actions: new Map<string, CardActionProps>([
-      ['address', { callback: undefined, params: null, description: 'Generate a receive address' }],
       [
-        'delegate',
+        'cosmos_getAddress',
+        { callback: undefined, params: null, description: 'Generate a receive address' },
+      ],
+      [
+        'cosmos_signTransaction',
         {
           callback: undefined,
           params: CosmosTransactions.delegate,
           description: 'Sign a Cosmos delegate message',
         },
       ],
-      [
-        'ibcTransfer',
-        {
-          callback: undefined,
-          params: CosmosTransactions.ibcTransfer,
-          description: 'Sign a Cosmos ibcTransfer message',
-        },
-      ],
-      [
-        'redelegate',
-        {
-          callback: undefined,
-          params: CosmosTransactions.redelegate,
-          description: 'Sign a Cosmos redelegate message',
-        },
-      ],
-      [
-        'rewards',
-        {
-          callback: undefined,
-          params: CosmosTransactions.rewards,
-          description: 'Sign a Cosmos rewards message',
-        },
-      ],
-      [
-        'transfer',
-        {
-          callback: undefined,
-          params: CosmosTransactions.transfer,
-          description: 'Sign a Cosmos transfer message',
-        },
-      ],
-      [
-        'undelegate',
-        {
-          callback: undefined,
-          params: CosmosTransactions.undelegate,
-          description: 'Sign a Cosmos undelegate message',
-        },
-      ],
     ]),
   },
   {
-    name: 'Ethereum',
+    name: 'Ethereum (ETH)',
     icon: 'eth.png',
     actions: new Map<string, CardActionProps>([
-      ['address', { callback: undefined, params: null, description: 'Generate a receive address' }],
-      ['send', { callback: undefined, params: EthereumTransactions.send }],
-      ['sign', { callback: undefined, params: EthereumTransactions.sign }],
-      ['tx', { callback: undefined, params: EthereumTransactions.tx }],
-      ['verify', { callback: undefined, params: EthereumTransactions.verify }],
+      [
+        'eth_getAddress',
+        { callback: undefined, params: null, description: 'Generate a receive address' },
+      ],
+      [
+        'eth_signMessage',
+        {
+          callback: undefined,
+          params: EthereumTransactions.sign,
+          description: 'Sign a test message',
+        },
+      ],
+      [
+        'eth_signTransaction',
+        {
+          callback: undefined,
+          params: EthereumTransactions.tx,
+          description: 'Sign a test transaction',
+        },
+      ],
+      [
+        'eth_verifyMessage',
+        {
+          callback: undefined,
+          params: EthereumTransactions.verify,
+          description: 'Verify the signature from a previously-signed test message',
+        },
+      ],
     ]),
   },
 ]
