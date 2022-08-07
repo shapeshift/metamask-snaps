6fbfd51fc7244723654f403484ae3c14b54cf2a1
pastaghost <pastaghost@2520.io>
2022-08-07 00:28:57 -0600

    feat: add ethereum rpc handlers

--- src/rpc/ethereum.ts
+++ src/rpc/ethereum.ts
@@ -1,42 +1,84 @@
 import {
+  bip32ToAddressNList,
   ETHSignedMessage,
   ETHSignedTx,
   ETHSignMessage,
   ETHSignTx,
   ETHVerifyMessage,
+  slip44ByCoin,
 } from '@shapeshiftoss/hdwallet-core'
 
 import { logger } from '../lib/logger'
+import { getHDWalletNativeSigner } from './common'
 
 const moduleLogger = logger.child({ namespace: ['Snap', 'Ethereum.ts'] })
 
-export const ethGetAddress = async (): Promise<string> => {
-  const ret = ''
-  moduleLogger.debug({ fn: 'ethGetAddress' }, `Executed ethGetAddress().`)
-  return ret
+export const ethGetAddress = async (account = 0, addressIndex = 0): Promise<string> => {
+  try {
+    const signer = await getHDWalletNativeSigner('Ethereum')
+    if (signer === null) {
+      throw new Error('Could not initialize Ethereum signer')
+    }
+    const address = await signer.ethGetAddress({
+      addressNList: bip32ToAddressNList(
+        `m/44'/${slip44ByCoin('Ethereum')}'/${account}'/0/${addressIndex}`,
+      ),
+      showDisplay: false,
+    })
+    if (address === null) {
+      throw new Error('Address generation failed')
+    }
+    return address
+  } catch (error) {
+    moduleLogger.error({ fn: 'ethGetAddress' }, error)
+    throw error
+  }
 }
 
 export const ethSignTransaction = async (transaction: ETHSignTx): Promise<ETHSignedTx> => {
-  const ret: ETHSignedTx = {
-    v: 0,
-    r: '',
-    s: '',
-    serialized: '',
+  try {
+    const signer = await getHDWalletNativeSigner('Ethereum')
+    if (signer === null) {
+      throw new Error('Could not initialize Ethereum signer')
+    }
+    const signedTransaction = await signer.ethSignTx(transaction)
+    if (signedTransaction === null) {
+      throw new Error('Transaction signing failed')
+    }
+    return signedTransaction
+  } catch (error) {
+    moduleLogger.error(transaction, { fn: 'ethSignTransaction' }, error)
+    throw error
   }
-  moduleLogger.debug(transaction, { fn: 'ethSignTransaction' }, `Executed ethSignTransaction().`)
-  return ret
 }
 
 export const ethSignMessage = async (message: ETHSignMessage): Promise<ETHSignedMessage> => {
-  const ret: ETHSignedMessage = {
-    address: '',
-    signature: '',
+  try {
+    const signer = await getHDWalletNativeSigner('Ethereum')
+    if (signer === null) {
+      throw new Error('Could not initialize Ethereum signer')
+    }
+    const signedMessage = await signer.ethSignMessage(message)
+    if (signedMessage === null) {
+      throw new Error('Transaction signing failed')
+    }
+    return signedMessage
+  } catch (error) {
+    moduleLogger.error(message, { fn: 'ethSignMessage' }, error)
+    throw error
   }
-  moduleLogger.debug(message, { fn: 'ethSignMessage' }, `Executed ethSignMessage().`)
-  return ret
 }
 
 export const ethVerifyMessage = async (message: ETHVerifyMessage): Promise<boolean> => {
-  moduleLogger.debug(message, { fn: 'ethVerifyMessage' }, `Executed ethVerifyMessage().`)
-  return false
+  try {
+    const signer = await getHDWalletNativeSigner('Ethereum')
+    if (signer === null) {
+      throw new Error('Could not initialize Ethereum signer')
+    }
+    const isVerified = await signer.ethVerifyMessage(message)
+    return isVerified
+  } catch (error) {
+    moduleLogger.error(message, { fn: 'ethVerifyMessage' }, error)
+    throw error
+  }
 }
