b4817a20b5dcd2f2c977800a8d11735cb3b2d50d
pastaghost <pastaghost@2520.io>
2022-08-07 00:29:05 -0600

    feat: add cosmos rpc handlers

--- src/rpc/cosmos.ts
+++ src/rpc/cosmos.ts
@@ -1,22 +1,50 @@
-import { CosmosSignedTx, CosmosSignTx } from '@shapeshiftoss/hdwallet-core'
+import {
+  bip32ToAddressNList,
+  CosmosSignedTx,
+  CosmosSignTx,
+  slip44ByCoin,
+} from '@shapeshiftoss/hdwallet-core'
 
 import { logger } from '../lib/logger'
+import { getHDWalletNativeSigner } from './common'
 
 const moduleLogger = logger.child({ namespace: ['Snap', 'Cosmos.ts'] })
 
-export const cosmosGetAddress = async (): Promise<string> => {
-  const ret = ''
-  moduleLogger.debug({ fn: 'cosmosGetAddress' }, `Executed cosmosGetAddress().`)
-  return ret
+export const cosmosGetAddress = async (account = 0, addressIndex = 0): Promise<string> => {
+  try {
+    const signer = await getHDWalletNativeSigner('Cosmos')
+    if (signer === null) {
+      throw new Error('Could not initialize Cosmos signer')
+    }
+    const address = await signer.cosmosGetAddress({
+      addressNList: bip32ToAddressNList(
+        `m/44'/${slip44ByCoin('Atom')}'/${account}'/0/${addressIndex}`,
+      ),
+      showDisplay: false,
+    })
+    if (address === null) {
+      throw new Error('Address generation failed')
+    }
+    return address
+  } catch (error) {
+    moduleLogger.error({ fn: 'cosmosGetAddress' }, error)
+    throw error
+  }
 }
 
 export const cosmosSignTransaction = async (transaction: CosmosSignTx): Promise<CosmosSignedTx> => {
-  const ret: CosmosSignedTx = {
-    serialized: '',
-    body: '',
-    authInfoBytes: '',
-    signatures: [''],
+  try {
+    const signer = await getHDWalletNativeSigner('Cosmos')
+    if (signer === null) {
+      throw new Error('Could not initialize Cosmos signer')
+    }
+    const signedTransaction = await signer.cosmosSignTx(transaction)
+    if (signedTransaction === null) {
+      throw new Error('Transaction signing failed')
+    }
+    return signedTransaction
+  } catch (error) {
+    moduleLogger.error(transaction, { fn: 'cosmosSignTransaction' }, error)
+    throw error
   }
-  moduleLogger.debug(transaction, { fn: 'cosmosSignTransaction' }, `Executed btcSignTx().`)
-  return ret
 }
