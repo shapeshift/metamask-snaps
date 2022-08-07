331c4fc0e1e11f8701d07c7966cb917525eb0bf7
pastaghost <pastaghost@2520.io>
2022-08-07 00:28:48 -0600

    feat: add bitcoin rpc handlers

--- src/rpc/bitcoin.ts
+++ src/rpc/bitcoin.ts
@@ -1,38 +1,91 @@
 import {
+  bip32ToAddressNList,
+  BTCInputScriptType,
   BTCSignedMessage,
   BTCSignedTx,
   BTCSignMessage,
   BTCSignTx,
   BTCVerifyMessage,
+  slip44ByCoin,
 } from '@shapeshiftoss/hdwallet-core'
 
 import { logger } from '../lib/logger'
+import { getHDWalletNativeSigner } from './common'
 
 const moduleLogger = logger.child({ namespace: ['Snap', 'Bitcoin.ts'] })
 
-export const btcGetAddress = async (): Promise<string> => {
-  const ret = ''
-  moduleLogger.debug({ fn: 'btcGetAddress' }, `Executed btcGetAddress().`)
-  return ret
+export const btcGetAddress = async (
+  scriptType: BTCInputScriptType,
+  account = 0,
+  addressIndex = 0,
+): Promise<string> => {
+  try {
+    const signer = await getHDWalletNativeSigner('Bitcoin')
+    if (signer === null) {
+      throw new Error('Could not initialize Bitcoin signer')
+    }
+    const address = await signer.btcGetAddress({
+      coin: 'Bitcoin',
+      addressNList: bip32ToAddressNList(
+        `m/44'/${slip44ByCoin('Cosmos')}'/${account}'/0/${addressIndex}`,
+      ),
+      scriptType,
+      showDisplay: false,
+    })
+    if (address === null) {
+      throw new Error('Address generation failed')
+    }
+    return address
+  } catch (error) {
+    moduleLogger.error({ fn: 'btcGetAddress' }, error)
+    throw error
+  }
 }
 
 export const btcSignTransaction = async (transaction: BTCSignTx): Promise<BTCSignedTx> => {
-  const ret: BTCSignedTx = {
-    signatures: [''],
-    serializedTx: '',
+  try {
+    const signer = await getHDWalletNativeSigner('Bitcoin')
+    if (signer === null) {
+      throw new Error('Could not initialize Bitcoin signer')
+    }
+    const signedTransaction = await signer.btcSignTx(transaction)
+    if (signedTransaction === null) {
+      throw new Error('Transaction signing failed')
+    }
+    return signedTransaction
+  } catch (error) {
+    moduleLogger.error(transaction, { fn: 'btcSignTransaction' }, error)
+    throw error
   }
-  moduleLogger.debug(transaction, { fn: 'btcSignTransaction' }, `Executed btcSignTransaction().`)
-  return ret
 }
 
-export const btcSignMessage = async (transaction: BTCSignMessage): Promise<BTCSignedMessage> => {
-  const ret: BTCSignedMessage = { address: '', signature: '' }
-  moduleLogger.debug(transaction, { fn: 'btcSignMessage' }, `Executed btcSignMessage().`)
-  return ret
+export const btcSignMessage = async (message: BTCSignMessage): Promise<BTCSignedMessage> => {
+  try {
+    const signer = await getHDWalletNativeSigner('Bitcoin')
+    if (signer === null) {
+      throw new Error('Could not initialize Bitcoin signer')
+    }
+    const signedMessage = await signer.btcSignMessage(message)
+    if (signedMessage === null) {
+      throw new Error('Transaction signing failed')
+    }
+    return signedMessage
+  } catch (error) {
+    moduleLogger.error(message, { fn: 'btcSignMessage' }, error)
+    throw error
+  }
 }
 
 export const btcVerifyMessage = async (message: BTCVerifyMessage): Promise<boolean> => {
-  const ret = false
-  moduleLogger.debug(message, { fn: 'btcVerifyMessage' }, `Executed btcVerifyMessage().`)
-  return ret
+  try {
+    const signer = await getHDWalletNativeSigner('Bitcoin')
+    if (signer === null) {
+      throw new Error('Could not initialize Bitcoin signer')
+    }
+    const isVerified = await signer.btcVerifyMessage(message)
+    return isVerified
+  } catch (error) {
+    moduleLogger.error(message, { fn: 'btcVerifyMessage' }, error)
+    throw error
+  }
 }
