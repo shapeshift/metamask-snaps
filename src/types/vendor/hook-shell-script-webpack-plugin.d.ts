6c37b688934d10912d9dabfbe1268dd5a33bf5a6
pastaghost <pastaghost@2520.io>
2022-08-08 00:49:55 -0600

    fix: resolve typescript module error

---
+++ src/types/vendor/hook-shell-script-webpack-plugin.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-default-export */
+// eslint-disable-next-line import/unambiguous
+declare module 'hook-shell-script-webpack-plugin' {
+  import { Compiler } from 'webpack'
+
+  export interface Options {
+    afterEmit: string[]
+  }
+
+  export default class HookShellScriptWebpackPlugin {
+    constructor(options: Options)
+
+    apply(compiler: Compiler): void
+  }
+}
