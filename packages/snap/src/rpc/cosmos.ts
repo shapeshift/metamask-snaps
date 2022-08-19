49e383d1f7064b02b1363a3f9e629173b729b3aa
pastaghost <pastaghost@2520.io>
2022-08-19 12:30:36 -0600

    chore: run linter

--- packages/snap/src/rpc/cosmos.ts
+++ packages/snap/src/rpc/cosmos.ts
@@ -3,48 +3,53 @@ import {
   CosmosSignedTx,
   CosmosSignTx,
   slip44ByCoin,
-} from '@shapeshiftoss/hdwallet-core'
+} from "@shapeshiftoss/hdwallet-core";
 
-import { logger } from '../lib/logger'
-import { getHDWalletNativeSigner } from './common'
+import { logger } from "../lib/logger";
+import { getHDWalletNativeSigner } from "./common";
 
-const moduleLogger = logger.child({ namespace: ['Snap', 'Cosmos.ts'] })
+const moduleLogger = logger.child({ namespace: ["Snap", "RPC", "Cosmos.ts"] });
 
-export const cosmosGetAddress = async (account = 0, addressIndex = 0): Promise<string> => {
+export const cosmosGetAddress = async (
+  account = 0,
+  addressIndex = 0
+): Promise<string> => {
   try {
-    const signer = await getHDWalletNativeSigner('Cosmos')
+    const signer = await getHDWalletNativeSigner("Cosmos");
     if (signer === null) {
-      throw new Error('Could not initialize Cosmos signer')
+      throw new Error("Could not initialize Cosmos signer");
     }
     const address = await signer.cosmosGetAddress({
       addressNList: bip32ToAddressNList(
-        `m/44'/${slip44ByCoin('Atom')}'/${account}'/0/${addressIndex}`,
+        `m/44'/${slip44ByCoin("Atom")}'/${account}'/0/${addressIndex}`
       ),
       showDisplay: false,
-    })
+    });
     if (address === null) {
-      throw new Error('Address generation failed')
+      throw new Error("Address generation failed");
     }
-    return address
+    return address;
   } catch (error) {
-    moduleLogger.error({ fn: 'cosmosGetAddress' }, error)
-    throw error
+    moduleLogger.error({ fn: "cosmosGetAddress" }, error);
+    throw error;
   }
-}
+};
 
-export const cosmosSignTransaction = async (transaction: CosmosSignTx): Promise<CosmosSignedTx> => {
+export const cosmosSignTransaction = async (
+  transaction: CosmosSignTx
+): Promise<CosmosSignedTx> => {
   try {
-    const signer = await getHDWalletNativeSigner('Cosmos')
+    const signer = await getHDWalletNativeSigner("Cosmos");
     if (signer === null) {
-      throw new Error('Could not initialize Cosmos signer')
+      throw new Error("Could not initialize Cosmos signer");
     }
-    const signedTransaction = await signer.cosmosSignTx(transaction)
+    const signedTransaction = await signer.cosmosSignTx(transaction);
     if (signedTransaction === null) {
-      throw new Error('Transaction signing failed')
+      throw new Error("Transaction signing failed");
     }
-    return signedTransaction
+    return signedTransaction;
   } catch (error) {
-    moduleLogger.error(transaction, { fn: 'cosmosSignTransaction' }, error)
-    throw error
+    moduleLogger.error(transaction, { fn: "cosmosSignTransaction" }, error);
+    throw error;
   }
-}
+};
