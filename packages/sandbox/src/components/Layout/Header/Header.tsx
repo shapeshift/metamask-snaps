5e49364a430e64fd2f6cc9135ad280c68a319de9
pastaghost <pastaghost@2520.io>
2022-08-30 14:32:45 -0600

    feat: add shapeshift logo

--- packages/sandbox/src/components/Layout/Header/Header.tsx
+++ packages/sandbox/src/components/Layout/Header/Header.tsx
@@ -1,11 +1,30 @@
-import { Center, Flex, Image } from '@chakra-ui/react'
+import { Box, Center, Flex, Image, Spacer, Stack } from '@chakra-ui/react'
 
 import { ConnectButton } from '../../ConnectButton/ConnectButton'
 
 export const Header = () => {
   return (
-    <Flex height='100%' width='100%' px='48px' flexDirection='row' justifyContent='space-between'>
-      <Image></Image>
+    <Flex height='100%' width='100%' pr='48px' flexDirection='row' justifyContent='space-between'>
+      <Box width={'200px'} pl='32px' height='100%'>
+        <Stack direction={'row'} width='100%' height='100%' spacing='16px' marginX='auto'>
+          <Image
+            objectFit='scale-down'
+            boxSize='100%'
+            width='100%'
+            height='50%'
+            marginY='auto'
+            src={require('../../../assets/shapeshift-fox-logo.png')}
+          />
+
+          <Image
+            objectFit='contain'
+            boxSize='100%'
+            marginY='auto'
+            src={require('../../../assets/shapeshift-text-logo.png')}
+          />
+        </Stack>
+      </Box>
+      <Spacer />
       <Center>
         <ConnectButton></ConnectButton>
       </Center>
