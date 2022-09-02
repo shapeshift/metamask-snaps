d4e368e6668fa0aa1569e81d09c19f10455214e7
pastaghost <pastaghost@2520.io>
2022-09-02 04:50:03 -0600

    feat: add supported broadcast rpc handlers

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,22 +1,10 @@
 /* eslint-disable no-unused-vars, @typescript-eslint/no-unused-vars */
-// import { OnRpcRequestHandler } from '@metamask/snap-types'
 
 import { OnRpcRequestHandler } from '@metamask/snap-types'
-// import { cosmosGetAddress, cosmosSignTransaction } from './rpc/cosmos'
-// import {
-//   dogeGetAddress,
-//   dogeSignMessage,
-//   dogeSignTransaction,
-//   dogeVerifyMessage,
-// } from './rpc/dogecoin'
-// import { ethGetAddress, ethSignMessage, ethSignTransaction, ethVerifyMessage } from './rpc/ethereum'
-// import { ltcGetAddress, ltcSignMessage, ltcSignTransaction, ltcVerifyMessage } from './rpc/litecoin'
-// import { thorchainGetAddress, thorchainSignTransaction } from './rpc/thorchain'
 import { ShapeShiftSnapRPCRequest } from '@shapeshiftoss/metamask-snaps-types'
 
 import { binanceGetAddress, binanceSignTransaction } from './rpc/binance'
-// import { binanceGetAddress, binanceSignTransaction } from './rpc/binance'
-import { btcGetAddress, btcSignMessage, btcSignTransaction, btcVerifyMessage } from './rpc/bitcoin'
+import { btcBroadcastTransaction, btcGetAddress, btcSignMessage, btcSignTransaction, btcVerifyMessage } from './rpc/bitcoin'
 import {
   bchBroadcastTransaction,
   bchGetAddress,
@@ -24,12 +12,6 @@ import {
   bchSignTransaction,
   bchVerifyMessage,
 } from './rpc/bitcoincash'
-// import {
-//   bchGetAddress,
-//   bchSignMessage,
-//   bchSignTransaction,
-//   bchVerifyMessage,
-// } from './rpc/bitcoincash'
 import { testFunction } from './rpc/common'
 import { cosmosBroadcastTransaction, cosmosGetAddress, cosmosSignTransaction } from './rpc/cosmos'
 import {
@@ -46,6 +28,7 @@ import {
   ethSignTransaction,
   ethVerifyMessage,
 } from './rpc/ethereum'
+import { kavaGetAddress, kavaSignTransaction } from './rpc/kava'
 import {
   ltcBroadcastTransaction,
   ltcGetAddress,
@@ -53,7 +36,9 @@ import {
   ltcSignTransaction,
   ltcVerifyMessage,
 } from './rpc/litecoin'
-import { osmosisGetAddress, osmosisSignTransaction } from './rpc/osmosis'
+import { osmosisBroadcastTransaction, osmosisGetAddress, osmosisSignTransaction } from './rpc/osmosis'
+import { secretGetAddress, secretSignTransaction } from './rpc/secret'
+import { terraGetAddress, terraSignTransaction } from './rpc/terra'
 import { thorchainGetAddress, thorchainSignTransaction } from './rpc/thorchain'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -86,15 +71,14 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
       return bchSignMessage(request.params.message)
     case 'bch_verifyMessage':
       return bchVerifyMessage(request.params.message)
-    // case "bch_broadcastTransaction":
-    // return bchBroadcastTransaction(request.params.transaction);
+    case "bch_broadcastTransaction":
+    return bchBroadcastTransaction(request.params.transaction);
     case 'binance_getAddress':
       return binanceGetAddress(request.params.params)
     case 'binance_signTransaction':
       return binanceSignTransaction(request.params.transaction)
     // case "binance_broadcastTransaction":
     //   return binanceBroadcastTransaction(request.params.transaction);
-
     case 'btc_getAddress':
       return btcGetAddress(request.params.params)
     case 'btc_signTransaction':
@@ -103,14 +87,14 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
       return btcSignMessage(request.params.message)
     case 'btc_verifyMessage':
       return btcVerifyMessage(request.params.message)
-    // case "btc_broadcastTransaction":
-    //   return btcBroadcastTransaction(request.params.transaction);
+    case "btc_broadcastTransaction":
+      return btcBroadcastTransaction(request.params.transaction);
     case 'cosmos_getAddress':
       return cosmosGetAddress(request.params.params)
     case 'cosmos_signTransaction':
       return cosmosSignTransaction(request.params.transaction)
-    // case "cosmos_broadcastTransaction":
-    //   return cosmosBroadcastTransaction(request.params.transaction);
+    case "cosmos_broadcastTransaction":
+      return cosmosBroadcastTransaction(request.params.transaction);
     case 'doge_getAddress':
       return dogeGetAddress(request.params.params)
     case 'doge_signTransaction':
@@ -129,8 +113,14 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
       return ethSignTransaction(request.params.transaction)
     case 'eth_verifyMessage':
       return ethVerifyMessage(request.params.message)
-    // case "eth_broadcastTransaction":
-    //   return ethBroadcastTransaction(request.params.transaction);
+    case "eth_broadcastTransaction":
+      return ethBroadcastTransaction(request.params.transaction);
+    case 'kava_getAddress':
+      return kavaGetAddress(request.params.params)
+    case 'kava_signTransaction':
+      return kavaSignTransaction(request.params.transaction)
+    // case "kava_broadcastTransaction":
+    //   return kavaBroadcastTransaction(request.params.transaction);
     case 'ltc_getAddress':
       return ltcGetAddress(request.params.params)
     case 'ltc_signTransaction':
@@ -145,16 +135,31 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
       return osmosisGetAddress(request.params.params)
     case 'osmosis_signTransaction':
       return osmosisSignTransaction(request.params.transaction)
-    // case "osmosis_broadcastTransaction":
-    //   return osmosisBroadcastTransaction(request.params.transaction);
+    case "osmosis_broadcastTransaction":
+      return osmosisBroadcastTransaction(request.params.transaction);
+    case 'secret_getAddress':
+      return secretGetAddress(request.params.params)
+    case 'secret_signTransaction':
+      return secretSignTransaction(request.params.transaction)
+    // case "secret_broadcastTransaction":
+    //   return secretBroadcastTransaction(request.params.transaction);
+    case 'terra_getAddress':
+      return terraGetAddress(request.params.params)
+    case 'terra_signTransaction':
+      return terraSignTransaction(request.params.transaction)
+    // case "terra_broadcastTransaction":
+    //   return terraBroadcastTransaction(request.params.transaction);
     case 'thorchain_getAddress':
       return thorchainGetAddress(request.params.params)
     case 'thorchain_signTransaction':
       return thorchainSignTransaction(request.params.transaction)
+    // case 'thorchain_broadcastTransaction':
+    //   return thorchainBroadcastTransaction(request.params.transaction)     
+    case 'ping':
+      return 'pong'
     case 'test':
       return testFunction()
-    // // case 'thorchain_broadcastTransaction':
-    // //   return thorchainBroadcastTransaction(request.params.transaction)
+
 
     default:
       throw new Error('Method not found.')
