14143a0da114a87be0affe3b8ae20ea1ced6e643
pastaghost <pastaghost@2520.io>
2022-09-02 04:51:35 -0600

    feat: add version check utilities

---
+++ packages/snap/src/utils.ts
@@ -0,0 +1,72 @@
+import { ExternalProvider } from '@ethersproject/providers'
+import detectEthereumProvider from '@metamask/detect-provider'
+
+import { logger } from './lib/logger'
+
+const moduleLogger = logger.child({ namespace: ['Snap', 'Utils.ts'] })
+
+const getMetaMaskProvider = async (): Promise<ExternalProvider> => {
+  try {
+    const ret = await detectEthereumProvider({ mustBeMetaMask: true })
+    return ret
+  } catch (error) {
+    moduleLogger.error(
+      error,
+      { fn: 'getMetaMaskProvider' },
+      'Please install MetaMask browser extension.',
+    )
+  }
+  return undefined
+}
+
+const getMetamaskVersion = async (): Promise<string> => {
+  try {
+    const provider = await getMetaMaskProvider()
+    return (await provider.request({
+      method: 'web3_clientVersion',
+      params: [],
+    })) as string
+  } catch (error) {
+    moduleLogger.error(error, { fn: 'getMetamaskVersion' }, 'Failed to get MetaMask version.')
+    return undefined
+  }
+}
+
+// Version comparison as implemented by ChainSafe: https://github.com/ChainSafe/filsnap/blob/master/packages/snap/src/util/version.ts
+const isNewerVersion = (current: string, comparingWith: string): boolean => {
+  if (current === comparingWith) return false
+
+  const regex = /[^\d.]/g
+  const currentFragments = current.replace(regex, '').split('.')
+  const comparingWithFragments = comparingWith.replace(regex, '').split('.')
+
+  const length =
+    currentFragments.length > comparingWithFragments.length
+      ? currentFragments.length
+      : comparingWithFragments.length
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < length; i++) {
+    if ((Number(currentFragments[i]) || 0) === (Number(comparingWithFragments[i]) || 0)) {
+      // eslint-disable-next-line no-continue
+      continue
+    }
+    return (Number(comparingWithFragments[i]) || 0) > (Number(currentFragments[i]) || 0)
+  }
+
+  return true
+}
+
+export const metaMaskVersionGreaterThanOrEqualTo = async (version: string): Promise<boolean> => {
+  try {
+    if (!version.includes('MetaMask/')) {
+      // eslint-disable-next-line no-param-reassign
+      version = `MetaMask/${version}`
+    }
+    const current = await getMetamaskVersion()
+    console.info('current version', version)
+    return isNewerVersion(current, version)
+  } catch (error) {
+    moduleLogger.error({ fn: 'metaMaskVersionGreaterThanOrEqualTo' }, error)
+    return false
+  }
+}
