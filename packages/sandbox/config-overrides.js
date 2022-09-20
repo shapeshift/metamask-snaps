74086d81776f8c6681163bf88afff5a106a90301
pastaghost <pastaghost@2520.io>
2022-09-20 07:59:57 -0600

    fix: add browser polyfills to sandbox build

---
+++ packages/sandbox/config-overrides.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    url: require.resolve('url'),
+    fs: require.resolve('fs'),
+    assert: require.resolve('assert'),
+    crypto: require.resolve('crypto-browserify'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser'),
+    buffer: require.resolve('buffer'),
+    stream: require.resolve('stream-browserify'),
+    path: require.resolve('path-browserify'),
+  }
+  config.module.rules = [
+    ...config.module.rules,
+    {
+      test: /\.m?js/,
+      resolve: {
+        fullySpecified: false,
+      },
+    },
+  ]
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  )
+
+  return config
+}
