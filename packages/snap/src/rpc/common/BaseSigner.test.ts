709d213c474929cdbe4fdc810b07b9e4eb9c878c
pastaghost <pastaghost@2520.io>
2022-09-21 18:24:16 -0600

    fix: document empty basesigner test

--- packages/snap/src/rpc/common/BaseSigner.test.ts
+++ packages/snap/src/rpc/common/BaseSigner.test.ts
@@ -1,1 +1,6 @@
-describe('BaseSigner', () => {})
+describe('BaseSigner', () => {
+    /** 
+     * The base signer class cannot be directly instantiated, 
+     * has no public methods exposed and so is not directly testable.
+    **/
+})
