12ed2b2a3a69e0340b00364e5dacbb2af8125d79
pastaghost <pastaghost@2520.io>
2022-09-30 13:30:51 -0600

    fix: typo in coin name

--- packages/sandbox/src/constants/transactions/BitcoinCashTransactions.ts
+++ packages/sandbox/src/constants/transactions/BitcoinCashTransactions.ts
@@ -2,7 +2,7 @@ import * as core from '@shapeshiftoss/hdwallet-core'
 
 export const BitcoinCashTransactions: any = {
   address: {
-    coin: 'Bitcoin Cash',
+    coin: 'BitcoinCash',
     addressNList: [0x80000000 + 44, 0x80000000 + 145, 0x80000000 + 0, 0, 0],
     scriptType: core.BTCInputScriptType.SpendAddress,
   },
